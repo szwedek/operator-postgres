c2b4c976e0f3495e13d2edc7720efee6f69ae651
Artur Szwed <arturszwed94@gmail.com>
2024-06-01 08:59:13 +0200

    Init commit

---
+++ test_operator.py
@@ -0,0 +1,18 @@
+import time
+import subprocess
+from kopf.testing import KopfRunner
+
+def test_operator():
+    with KopfRunner(['run', '-A', '--verbose', 'operator.py']) as runner:
+        # do something while the operator is running.
+
+        subprocess.run("kubectl apply -f examples/obj.yaml", shell=True, check=True)
+        time.sleep(1)  # give it some time to react and to sleep and to retry
+
+        subprocess.run("kubectl delete -f examples/obj.yaml", shell=True, check=True)
+        time.sleep(1)  # give it some time to react
+
+    assert runner.exit_code == 0
+    assert runner.exception is None
+    assert 'And here we are!' in runner.stdout
+    assert 'Deleted, really deleted' in runner.stdout
